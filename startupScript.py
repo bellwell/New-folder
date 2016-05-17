c9badc6d9e57d25e334b2283e865454313d9d7d4
bellwell <davidquach.aus@gmail.com>
2016-05-17 13:31:43 -0500

    testing
    1 2 3

---
+++ startupScript.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 17 09:34:00 2016
+
+@author: quacda01
+"""
+
+#%% startup
+
+from __future__ import division
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime, date, time
+
+from dateutil.parser import parse
+
+import math
+
+from scipy import stats
+
+import time as t
+
+
+#%%
+
+
+
+
+
